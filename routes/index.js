438d69a8d74ab35223329c6ea2c439d4e410b6f4
imgomi <webmaster@mail.gomi.land>
2020-09-06 22:25:08 +0900

    passport.js 로그아웃

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var userData = req.user;
+  res.status(200).json({
+    userData
+  })
 });
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+})
 
 module.exports = router;
